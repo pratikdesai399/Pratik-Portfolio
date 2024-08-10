862b637f4fa3b2569c68887a82dbd18941d264d0
pratikdesai399 <pratikdesai399@gmail.com>
2024-08-10 17:02:02 -0500

    Updates

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,40 +1,36 @@
 import Project1 from "../../assets/project1.webp";
 import Project2 from "../../assets/project2.webp";
 import Project3 from "../../assets/project3.webp";
+import Project4 from "../../assets/project3.webp";
 
 
 export const projectsData = [
     {
         id: 1,
         image: Project1,
-        title: "Project 1",
+        title: "StackSense",
         category: "A",
     },
     {
         id: 2,
         image: Project2,
-        title: "Project 2",
+        title: "WhatsNxt",
         category: "B",
     },
     {
         id: 3,
         image: Project3,
-        title: "Project 3",
+        title: "Employee Management System",
         category: "C",
     },
+    {
+        id: 4,
+        image: Project4,
+        title: "Course Evaluation Summarizer",
+        category: "D",
+    }
 ];
 
 export const projectsNav = [
-    {
-        name: "All",
-    },
-    {
-        name: "A",
-    },
-    {
-        name: "B",
-    },
-    {
-        name: "C",
-    },
+    
 ];
